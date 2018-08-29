f5520ffa98a8d1eaebc440e1783f3da3335a1b46
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-29 10:21:14 -0500

    creando un ejemplo de practica

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* POST users listing. */
 router.post('/', function(req, res, next) {
   var user = req.body.user;
   var passwd = req.body.passwd;
   if( user == "pepito" && passwd == "fuentes"){
     res.redirect("/form");
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : ''
+    });
+
+    connection.connect();
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+      if (err) throw err;
+      console.log('The solution is: ', rows[0].solution);
+    });
+    connection.end();
   }else{
     res.redirect("/")
   }
