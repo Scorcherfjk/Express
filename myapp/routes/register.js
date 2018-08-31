27f81fc38cf5a84308b467f3922bbaa18083a40d
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-31 17:45:03 -0500

    rutas mejor ubicadas

---
+++ myapp/routes/register.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.get('/natural-person', function(req, res, next) {
+  res.render('naturalPerson', { title: 'Registro - Persona Natural' });
+});
+
+router.get('/ruc', function(req, res, next) {
+  res.render('ruc', { title: 'Registro - RUC' });
+});
+
+router.get('/evaluator', function(req, res, next) {
+  res.render('evaluator', { title: 'Registro - Evaluador' });
+});
+
+
+module.exports = router;
