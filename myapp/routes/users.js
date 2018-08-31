b9cf912bde048b3c4d82f236a29ad269df78ef35
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-31 17:46:35 -0500

    prueba de conexion

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,28 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
-/* POST users listing. */
-router.post('/', function(req, res, next) {
-  var user = req.body.user;
-  var passwd = req.body.passwd;
-  if( user == "pepito" && passwd == "fuentes"){
-    res.redirect("/form");
-    var mysql      = require('mysql');
-    var connection = mysql.createConnection({
-      host     : 'localhost',
-      user     : 'root',
-      password : ''
-    });
+/*
+var Conn = require('tedious');
 
-    connection.connect();
-    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-      if (err) throw err;
-      console.log('The solution is: ', rows[0].solution);
-    });
-    connection.end();
-  }else{
-    res.redirect("/")
-  }
+var config = {
+    userName: 'FTMUser',
+    password: 'FTMUser',
+    server: 'W2K12XPOC',
+    
+    options: {
+        encrypt: false,
+        database: 'FTMetrics2'
+    }
+};
+
+var conn = new Conn.Connection(config);
+conn.on('connect', function(err) {
+    if(err){
+        res.send(err);
+    }else{
+        console.log("Connected"); 
+    }
 });
+*/
+/* POST users listing. */
+router.post('/', function(req, res) {
+
+    var user = req.body.user;
+    var passwd = req.body.passwd;
+
+    if( user == "pepito" && passwd == "fuentes"){
+        res.redirect("/form");   
+    }else{
+        res.redirect('/');
+    }
+});
+
+/*
+router.get('/', function(req,res){
+    
+    var conn = new Conn.Connection(config);
+    var request = new Conn.Request("SELECT * FROM FTMetrics2.dbo.RptReportInventory;", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+    conn.on('connect', function(err) {
+        if(err){
+            res.send(err);
+        }else{ 
+            request.on('row', function(columns) {    
+                var val = columns[0].value; 
+                res.send(val);     
+            });
+            conn.execSql(request);
+        }
+    });
+});
+*/
 
 module.exports = router;
