1df1e11042f0399b98057f14e22bd1b0478e769c
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-09-05 11:51:57 -0500

    separando base de datos

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,62 +1,66 @@
 var express = require('express');
 var router = express.Router();
+var Conn = require('tedious').Connection;
+var Request = require('tedious').Request;
+var config = require('../models/database').config;
 
-/*
-var Conn = require('tedious');
+function conexion(conn) {
+    conn.on('connect', function(err) {
+        if(err){
+            console.log(err)
+        }else{
+            console.log("Connected"); 
+        }
+    });
+}
 
-var config = {
-    userName: 'FTMUser',
-    password: 'FTMUser',
-    server: 'W2K12XPOC',
-    
-    options: {
-        encrypt: false,
-        database: 'FTMetrics2'
-    }
-};
+var conn = new Conn(config());
+try {
+    conexion(conn);
+} catch (ConnectionError) {
+    conexion(conn);
+}
 
-var conn = new Conn.Connection(config);
-conn.on('connect', function(err) {
-    if(err){
-        res.send(err);
-    }else{
-        console.log("Connected"); 
-    }
-});
-*/
+
 /* POST users listing. */
 router.post('/', function(req, res) {
 
     var user = req.body.user;
     var passwd = req.body.passwd;
+    var sql = 'SELECT usuario, clave from Huacho.dbo.Users';
+    var result = [];
 
-    if( user == "pepito" && passwd == "fuentes"){
-        res.redirect("/form");   
-    }else{
-        res.redirect('/');
-    }
-});
-
-/*
-router.get('/', function(req,res){
-    
-    var conn = new Conn.Connection(config);
-    var request = new Conn.Request("SELECT * FROM FTMetrics2.dbo.RptReportInventory;", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-    conn.on('connect', function(err) {
-        if(err){
-            res.send(err);
-        }else{ 
-            request.on('row', function(columns) {    
-                var val = columns[0].value; 
-                res.send(val);     
-            });
-            conn.execSql(request);
+    var request = new Request(sql, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        for (let i = 0; i < result.length; i++) {
+            var valor = result[i];
+            if( user == valor.usuario && passwd == valor.clave){
+                console.log("Acceso concedido");
+                var a = true;
+            }
+        }
+        if (a) {
+            res.redirect("/form");
+        }else{
+            res.redirect("/");
         }
     });
+    request.on("row", function (columns) { 
+        var item = {}; 
+        columns.forEach(function (column) { 
+            item[column.metadata.colName] = column.value; 
+        }); 
+        result.push(item);
+    });
+
+    conn.execSql(request);
 });
-*/
+
+
+router.get('/', function(req,res){
+    res.redirect("/");
+});
 
 module.exports = router;
