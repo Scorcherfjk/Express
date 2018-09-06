4a0c4b6fe12262319bf2dae0cea6513004981523
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-09-06 14:30:18 -0500

    innecesarios

--- myapp/routes/register.js
+++ myapp/routes/register.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/');
+  res.render('register', { title: 'Registro' });
 });
-
-router.get('/natural-person', function(req, res, next) {
-  res.render('naturalPerson', { title: 'Registro | Persona Natural' });
-});
-
-router.get('/ruc', function(req, res, next) {
-  res.render('ruc', { title: 'Registro | RUC' });
-});
-
-router.get('/evaluator', function(req, res, next) {
-  res.render('evaluator', { title: 'Registro | Evaluador' });
-});
-
 
 module.exports = router;
