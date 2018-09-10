60d297aa1611b2d747f7bb18060a01f5841d12d4
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-09-10 11:48:50 -0500

    vamos por la carga de datos

--- myapp/routes/validation.js
+++ myapp/routes/validation.js
@@ -23,7 +23,7 @@ router.post('/', function(req, res) {
 
     var user = req.body.user;
     var passwd = req.body.passwd;
-    var sql = 'SELECT usuario, clave from Huacho.dbo.Users';
+    var sql = 'SELECT usuario, clave from unjfsc.dbo.usuarios';
     var result = [];
 
     var request = new Request(sql, function(err) {
@@ -42,7 +42,7 @@ router.post('/', function(req, res) {
             res.redirect("/");
         } else {
             res.locals = { user: req.session.user};
-            res.redirect('/form');
+            res.redirect('/inicio');
         }
     });
     request.on("row", function (columns) { 
@@ -59,5 +59,38 @@ router.post('/', function(req, res) {
 router.get('/', function(req,res){
     res.redirect("/");
 });
+
+router.post("/new-user", function (res,req) {
+    var new_user = {
+                    "tipo_documento" : req.body.tipo_documento,
+                    "documento_identidad" : req.body.documento_identidad,
+                    "nombres" : req.body.nombres,
+                    "apellido_paterno" : req.body.apellido_paterno,
+                    "apellido_materno" : req.body.apellido_materno,
+                    "genero" : req.body.genero,
+                    "pais" : req.body.pais,
+                    "departamento" : req.body.departamento,
+                    "provincia" : req.body.provincia,
+                    "distrito" : req.body.distrito,
+                    "direccion" : req.body.direccion,
+                    "fecha_nacimiento" : req.body.fecha_nacimiento,
+                    "telefono_movil" : req.body.telefono_movil,
+                    "telefono_fijo" : req.body.telefono_fijo,
+                    "email" : req.body.email,
+                    "email2" : req.body.email2,
+                    "docente" : req.body.docente,
+                    "usuario" : req.body.usuario,
+                    "clave" : req.body.clave,
+                    "confirmarClave" : req.body.confirmarClave,
+                }
+    if(new_user.clave == new_user.confirmarClave){
+        var sql = "INSERT INTO [unjfsc].[dbo].[usuario] ([tipo_documento], [documento_identidad], [nombres], [apellido_paterno], [apellido_materno], [genero], [pais], [departamento], [provincia], [distrito], [direccion], [fecha_nacimiento], [telefono_movil], [telefono_fijo], [email], [email2], [docente], [usuario], [clave])" +
+        "VALUES (" + req.body.tipo_documento + ", " + new_user.documento_identidad + ", " + new_user.nombres + ", " + new_user.apellido_paterno + ", " + new_user.apellido_materno + ", " + new_user.genero + ", " + new_user.pais + ", " + new_user.departamento + ", " + new_user.provincia + ", " + new_user.distrito + ", " + new_user.direccion + ", " + new_user.fecha_nacimiento + ", " + new_user.telefono_movil + ", " + new_user.telefono_fijo + ", " + new_user.email + ", " + new_user.email2 + ", " + new_user.docente + ", " + new_user.usuario + ", " + new_user.clave+ ")";
+    }else{
+        res.redirect("/");
+    }
+});
+
+
 
 module.exports = router;
