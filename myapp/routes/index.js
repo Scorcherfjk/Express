7692a15ddc9b8c7d256813fd492ee0aee50ddee6
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-09-11 16:48:39 -0500

    listo el insert del registro

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Conn = require('tedious').Connection;
 var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;  
 var config = require('../models/database').config;
 
 
@@ -18,12 +19,71 @@ function conexion(conn) {
 var conn = new Conn(config());
 conexion(conn);
 
+
+/********************************************             INDEX              *************************************************/
+router.get('/', function(req, res, next) {
+    req.session.destroy( function (err) {
+      if(err){
+        console.log(err);
+      }else{
+        console.log("sesion destruida");
+      }
+    });
+    res.render('index', { title: 'Universidad jfsc' });
+  });
+  
+  router.get('/register', function(req, res, next) {
+    res.render('register', { title: 'Registro de usuario' });
+  });
+/*******************************************************************************************************************************/
+
+
+
+/*****************************************             ACTIVE SESSION          *************************************************/
+router.get('/inicio' ,function(req, res, next) {
+    if(req.session.user){
+      res.render('inicio', { title: "Bienvenido | Universidad jfsc", usuario: req.session.user });
+    } else {
+      res.redirect("/");
+    }
+  });
+  
+  router.get('/form' ,function(req, res, next) {
+    if(req.session.user){
+      res.render('form', { title: "Formulario", usuario: req.session.user });
+    } else {
+      res.redirect("/");
+    }
+  });
+  
+  router.get('/change-password' ,function(req, res, next) {
+    if(req.session.user){
+      res.render('changePassword', { title: "Cambio de Clave", usuario: req.session.user });
+  } else {
+    res.redirect("/");
+  }
+  });
+  
+  router.get('/change-data' ,function(req, res, next) {
+    if(req.session.user){
+      res.render('changeData', { title: "Cambio de Datos", usuario: req.session.user });
+  } else {
+    res.redirect("/");
+  }
+  });
+  
+/*******************************************************************************************************************************/
+
+
 /**********************************************         DATABASE             ***************************************************/
+
+
+/********************************** INICIO DE SESION*****************/
 router.post('/validation', function(req, res) {
 
     var user = req.body.user;
     var passwd = req.body.passwd;
-    var sql = 'SELECT usuario, clave from unjfsc.dbo.usuarios';
+    var sql = 'SELECT usuario, clave from unjfsc.dbo.usuario';
     var result = [];
 
     var request = new Request(sql, function(err) {
@@ -41,7 +101,6 @@ router.post('/validation', function(req, res) {
             console.log(req.session.user);
             res.redirect("/");
         } else {
-            res.locals.user = req.session.user;
             res.redirect('/inicio');
         }
     });
@@ -56,91 +115,78 @@ router.post('/validation', function(req, res) {
     conn.execSql(request);
 });
 
-router.post("/validation/new-user", function (res,req) {
-    var new_user = {
-                    "tipo_documento" : req.body.tipo_documento,
-                    "documento_identidad" : req.body.documento_identidad,
-                    "nombres" : req.body.nombres,
-                    "apellido_paterno" : req.body.apellido_paterno,
-                    "apellido_materno" : req.body.apellido_materno,
-                    "genero" : req.body.genero,
-                    "pais" : req.body.pais,
-                    "departamento" : req.body.departamento,
-                    "provincia" : req.body.provincia,
-                    "distrito" : req.body.distrito,
-                    "direccion" : req.body.direccion,
-                    "fecha_nacimiento" : req.body.fecha_nacimiento,
-                    "telefono_movil" : req.body.telefono_movil,
-                    "telefono_fijo" : req.body.telefono_fijo,
-                    "email" : req.body.email,
-                    "email2" : req.body.email2,
-                    "docente" : req.body.docente,
-                    "usuario" : req.body.usuario,
-                    "clave" : req.body.clave,
-                    "confirmarClave" : req.body.confirmarClave,
-                }
-    if(new_user.clave == new_user.confirmarClave){
-        var sql = "INSERT INTO [unjfsc].[dbo].[usuario] ([tipo_documento], [documento_identidad], [nombres], [apellido_paterno], [apellido_materno], [genero], [pais], [departamento], [provincia], [distrito], [direccion], [fecha_nacimiento], [telefono_movil], [telefono_fijo], [email], [email2], [docente], [usuario], [clave])" +
-        "VALUES (" + req.body.tipo_documento + ", " + new_user.documento_identidad + ", " + new_user.nombres + ", " + new_user.apellido_paterno + ", " + new_user.apellido_materno + ", " + new_user.genero + ", " + new_user.pais + ", " + new_user.departamento + ", " + new_user.provincia + ", " + new_user.distrito + ", " + new_user.direccion + ", " + new_user.fecha_nacimiento + ", " + new_user.telefono_movil + ", " + new_user.telefono_fijo + ", " + new_user.email + ", " + new_user.email2 + ", " + new_user.docente + ", " + new_user.usuario + ", " + new_user.clave+ ")";
-    }else{
-        res.redirect("/");
-    }
+
+/*********************************REGISTRO DE NUEVO USUARIO*****************/
+router.post("/validation/new-user", function (req,res) {
+
+    var tipo_documento = req.body.tipo_documento;
+    var documento_identidad = req.body.documento_identidad;
+    var nombres = req.body.nombres;
+    var apellido_paterno = req.body.apellido_paterno;
+    var apellido_materno = req.body.apellido_materno;
+    var genero = req.body.genero;
+    var pais = req.body.pais;
+    var departamento = req.body.departamento;
+    var provincia = req.body.provincia;
+    var distrito = req.body.distrito;
+    var direccion = req.body.direccion;
+    var fecha_nacimiento = req.body.fecha_nacimiento;
+    var telefono_movil = req.body.telefono_movil;
+    var telefono_fijo = req.body.telefono_fijo;
+    var email = req.body.email;
+    var email2 = req.body.email2;
+    var docente = req.body.docente;
+    var usuario = req.body.email;
+    var clave = req.body.clave;
+    
+   var sql = "INSERT INTO [unjfsc].[dbo].[usuario] ([tipo_documento], [documento_identidad], [nombres], [apellido_paterno], [apellido_materno], [genero], [pais], [departamento], [provincia], [distrito], [direccion], [fecha_nacimiento], [telefono_movil], [telefono_fijo], [email], [email2], [docente], [usuario], [clave]) OUTPUT INSERTED.id_usuario VALUES ( @tipo_documento, @documento_identidad, @nombres, @apellido_paterno, @apellido_materno, @genero, @pais, @departamento, @provincia, @distrito, @direccion, @fecha_nacimiento, @telefono_movil, @telefono_fijo, @email, @email2, @docente, @usuario, @clave)";
+    
+    request = new Request(sql, function(err) {  
+        if (err) {  
+           console.log(err);
+        }
+        req.session.user = usuario;
+        console.log("Acceso concedido");
+        if(!req.session.user){
+            console.log(req.session.user);
+            res.redirect("/");
+        } else {
+            res.redirect('/inicio');
+        }
+
+       });
+
+    request.addParameter("tipo_documento" ,         TYPES.Int           , tipo_documento);
+    request.addParameter("documento_identidad" ,    TYPES.Int           , documento_identidad);
+    request.addParameter("nombres" ,                TYPES.VarChar       , nombres);
+    request.addParameter("apellido_paterno" ,       TYPES.VarChar       , apellido_paterno);
+    request.addParameter("apellido_materno" ,       TYPES.VarChar       , apellido_materno);
+    request.addParameter("genero" ,                 TYPES.Int           , genero);
+    request.addParameter("pais" ,                   TYPES.VarChar       , pais);
+    request.addParameter("departamento" ,           TYPES.VarChar       , departamento);
+    request.addParameter("provincia" ,              TYPES.VarChar       , provincia);
+    request.addParameter("distrito" ,               TYPES.VarChar       , distrito);
+    request.addParameter("direccion" ,              TYPES.VarChar       , direccion);
+    request.addParameter("fecha_nacimiento" ,       TYPES.Date          , fecha_nacimiento);
+    request.addParameter("telefono_movil" ,         TYPES.Int           , telefono_movil);
+    request.addParameter("telefono_fijo" ,          TYPES.Int           , telefono_fijo);
+    request.addParameter("email" ,                  TYPES.VarChar       , email);
+    request.addParameter("email2" ,                 TYPES.VarChar       , email2);
+    request.addParameter("docente" ,                TYPES.Int           , docente);
+    request.addParameter("usuario" ,                TYPES.VarChar       , usuario);
+    request.addParameter("clave" ,                  TYPES.VarChar       , clave);
+        
+    request.on('row', function(columns) {  
+        columns.forEach(function(column) {  
+          if (column.value === null) {  
+            console.log('NULL');  
+          } else {  
+            console.log("User id is " + column.value);  
+          }  
+        });  
+    });       
+    conn.execSql(request);
 });
-
-
-/********************************************             INDEX              *************************************************/
-router.get('/', function(req, res, next) {
-  req.session.destroy( function (err) {
-    if(err){
-      console.log(err);
-    }else{
-      console.log("sesion destruida");
-    }
-  });
-  res.render('index', { title: 'Universidad jfsc' });
-});
-
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Registro de usuario' });
-});
-/*******************************************************************************************************************************/
-
-
-
-/*****************************************             ACTIVE SESSION          *************************************************/
-router.get('/inicio' ,function(req, res, next) {
-  if(req.session.user){
-    res.render('inicio', { title: "Bienvenido | Universidad jfsc" });
-  } else {
-    res.redirect("/");
-  }
-});
-
-router.get('/form' ,function(req, res, next) {
-  if(req.session.user){
-    res.render('form', { title: "Formulario" });
-  } else {
-    res.redirect("/");
-  }
-});
-
-router.get('/change-password' ,function(req, res, next) {
-  if(req.session.user){
-    res.render('changePassword', { title: "Cambio de Clave" });
-} else {
-  res.redirect("/");
-}
-});
-
-router.get('/change-data' ,function(req, res, next) {
-  if(req.session.user){
-    res.render('changeData', { title: "Cambio de Datos" });
-} else {
-  res.redirect("/");
-}
-});
-
-/*******************************************************************************************************************************/
 
 
 /*******************************************             FILTERING          ****************************************************/
